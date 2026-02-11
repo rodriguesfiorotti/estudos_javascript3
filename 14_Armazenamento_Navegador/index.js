d202fc6f80ced167629065fde3ffc58de8e09021
João Victor Rodrigues <rodriguesfiorotti@gmail.com>
2026-02-10 21:02:01 -0300

    Exercicio tic-tac-toe e Armazenamentos do Navegador

---
+++ 14_Armazenamento_Navegador/index.js
@@ -0,0 +1,53 @@
+// ------------------------------------------------------
+// USANDO SESSION STORAGE:
+
+document.getElementById('sessionBtn').addEventListener('click', function () {
+  const input = document.getElementById('session')
+  sessionStorage.setItem('info', input.value)
+  input.value = ''
+})
+ 
+document.getElementById('readSesssion').addEventListener('click', function () {
+  const info = sessionStorage.getItem('info')
+  alert('A informação salva é: ' + info)
+})
+
+
+// ------------------------------------------------------
+// USANDO LOCAL STORAGE:
+
+document.getElementById('localBtn').addEventListener('click', function () {
+  const input = document.getElementById('local')
+  localStorage.setItem('text', input.value)
+  input.value = ''
+})
+
+document.getElementById('readLocal').addEventListener('click', function () {
+  const t = localStorage.getItem('text')
+  alert('O texto salvo no local storage é: ' + t)
+})
+
+// ------------------------------------------------------
+// USANDO COOKIES:
+
+document.getElementById('cookieBtn').addEventListener('click', function () {
+  const input = document.getElementById('cookie')
+  // cookieName=value; expires=UTCStringDate; path=/;
+  const cookie = 'info=' + input.value + ';'
+  const expiration = 'expires=' + new Date(2027,3, 3) + ';'
+  const path = 'path=/;'
+  document.cookie = cookie + expiration + path
+  input.value = ''
+  console.log(document.cookie)
+})
+
+document.getElementById('cookie2Btn').addEventListener('click', function () {
+  const input = document.getElementById('cookie2')
+  // cookieName=value; expires=UTCStringDate; path=/;
+  const cookie = 'text=' + input.value + ';'
+  const expiration = 'expires=' + new Date(2027, 3, 3) + ';'
+  const path = 'path=/;'
+  document.cookie = cookie + expiration + path
+  input.value = ''
+  console.log(document.cookie)
+})
